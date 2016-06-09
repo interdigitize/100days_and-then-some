d1adeebeff83da4a779319fc31b2d7d77b0a0978
blikethat <kamie.lyn@gmail.com>
2016-06-08 22:08:58 -0700

    not elegant, but solved -- good enough until I learn a better way

--- day49-coderbyte-stringPracticeProblems.js
+++ day49-coderbyte-stringPracticeProblems.js
@@ -1,16 +1,32 @@
 //Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
 
-function LetterChanges(str){
-  str = str.split("");
-  return str.map(function(letter, index, array){
-    letter = String.fromCharCode(array[index].charCodeAt(letter)+1);
-    if(letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u")
-    // if(letter === /(a|e|i|o|u)/) this doesn't work
-    {
-      letter = letter.toUpperCase()
-    }
-    return letter;
-  }).join("");
-}
+//SOLVED with for loops
 
-console.log(LetterChanges("dweet dwe dow"));
+function LetterChanges(str){
+  var changeChar = "", answer = "";
+  for (var i = 0; i <str.length; i++){
+    changeChar += str[i].replace(/[a-y]/gi, String.fromCharCode(str.charCodeAt(i)+1));
+  }
+  changeChar = changeChar.replace("z", "a");
+  for (var i = 0; i <str.length; i++){
+    answer += changeChar[i].replace(/(a|e|i|o|u)/g, changeChar[i].toUpperCase());
+  }
+  return answer;
+}
+console.log(LetterChanges("dun dun duh"));
+
+
+
+// function LetterChanges(str){
+//   str = str.split("");
+//   return str.map(function(letter, index, array){
+//     letter = String.fromCharCode(array[index].charCodeAt(letter)+1);
+//     if(letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u")
+//     {
+//       letter = letter.toUpperCase()
+//     }
+//     return letter;
+//   }).join("");
+// }
+//
+// console.log(LetterChanges("dweet dwe dow"));
