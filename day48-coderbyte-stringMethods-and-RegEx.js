2d472ae558fcaa26e1fc2a787149b738cd84cf40
blikethat <kamie.lyn@gmail.com>
2016-06-11 15:57:56 -0700

    added regex details

--- day48-coderbyte-stringMethods-and-RegEx.js
+++ day48-coderbyte-stringMethods-and-RegEx.js
@@ -57,3 +57,43 @@ console.log(wor);
 var str = "ABxxAxxBABxxAxxB";
 var pos = str.search(/A..B/gi);
 console.log(pos);
+
+//RegEx http://eloquentjavascript.net/09_regexp.html
+// \d	Any digit character
+// \w	An alphanumeric character (“word character”)
+// \s	Any whitespace character (space, tab, newline, and similar)
+// \D	A character that is not a digit
+// \W	A nonalphanumeric character
+// \S	A nonwhitespace character
+// \b. A word boundary can be the start or end of the string or any point in the string that has a word character (as in \w) on one side and a nonword character on the other.
+// .	Any character except for newline
+
+// Knowing precisely what characters to backslash-escape when writing regular expressions requires you to know every character with a special meaning. For the time being, this may not be realistic, so when in doubt, just put a backslash before any character that is not a letter, number, or whitespace.
+
+// When you put a plus sign (+) after something in a regular expression, it indicates that the element may be repeated more than once. Thus, /\d+/ matches one or more digit characters.
+// The star (*) has a similar meaning but also allows the pattern to match zero times. Something with a star after it never prevents a pattern from matching—it’ll just match zero instances if it can’t find any suitable text to match.
+//A question mark makes a part of a pattern “optional”, meaning it may occur zero or one time.
+//To indicate that a pattern should occur a precise number of times, use curly braces. Putting {4} after an element, for example, requires it to occur exactly four times. It is also possible to specify a range this way: {2,4} means the element must occur at least twice and at most four times.
+//To invert a set of characters—that is, to express that you want to match any character except the ones in the set—you can write a caret (^) character after the opening bracket.
+
+//regex METHODS = test, exec, match
+
+//EXAMPLES
+// /abc/	A sequence of characters
+// /[abc]/	Any character from a set of characters
+// /[^abc]/	Any character not in a set of characters
+// /[0-9]/	Any character in a range of characters
+// /x+/	One or more occurrences of the pattern x
+// /x+?/	One or more occurrences, nongreedy
+// /x*/	Zero or more occurrences
+// /x?/	Zero or one occurrence
+// /x{2,4}/	Between two and four occurrences
+// /(abc)/	A group
+// /a|b|c/	Any one of several patterns
+// /\d/	Any digit character
+// /\w/	An alphanumeric character (“word character”)
+// /\s/	Any whitespace character
+// /./	Any character except newlines
+// /\b/	A word boundary
+// /^/	Start of input
+// /$/	End of input
