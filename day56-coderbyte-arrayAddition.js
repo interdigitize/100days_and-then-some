f318fc5c6032d763528f8a80adfb7c6055c5d31f
blikethat <kamie.lyn@gmail.com>
2016-06-15 08:09:40 -0700

    Not my favorite chunck of code because if seems incomplete, but sometimes when it passes all the tests, you just say okay and move on.

---
+++ day56-coderbyte-arrayAddition.js
@@ -0,0 +1,60 @@
+//Using the JavaScript language, have the function ArrayAdditionI(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers.
+
+function ArrayAdditionI(arr) {
+  var sortedArr = arr.sort(function(a,b){return a - b;}).reverse();
+  var largestNum = sortedArr.shift();
+  var arrSum = sortedArr.reduce(function(previousNum, currentNum){
+    return previousNum + currentNum;
+  });
+  function allButOne(arr2){
+     for(var i = 0; i < arr2.length; i++){
+      var newSum = arrSum - arr2[i];
+      if(newSum === largestNum){
+        return newSum;
+      }
+    }
+    return false;
+  }
+
+  function twoNums(arr2){
+    for(var i = 1; i < arr2.length; i++){
+      var newSum = arr2[0] + arr2[i];
+      if(newSum === largestNum){
+        return newSum;
+      }
+    }
+    return false;
+  }
+
+  function threeNums(arr2){
+    for(var i = 2; i < arr2.length; i++){
+      var newSum = arr2[0] + arr2[i] + arr2[i-1];
+      if(newSum === largestNum){
+        return newSum;
+      }
+    }
+    return false;
+  }
+
+  if (largestNum === arrSum){
+    return true;
+  }
+  else if(largestNum === allButOne(sortedArr)){
+    return true;
+  }
+  else if (largestNum === twoNums(sortedArr)){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+}
+
+console.log(ArrayAdditionI([10,2,16,1,5]));
+
+// Input:5,7,16,1,2
+// Output:"false"
+//
+// Input:3,5,-1,8,12
+// Output:"true"
