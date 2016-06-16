2c993d3cd3035ae6ea0346148baf2484557548f6
blikethat <kamie.lyn@gmail.com>
2016-06-16 07:27:39 -0700

    ...added commented out logic explaining a more savvy solution

--- day56-coderbyte-arrayAddition.js
+++ day56-coderbyte-arrayAddition.js
@@ -1,5 +1,7 @@
 //Using the JavaScript language, have the function ArrayAdditionI(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers.
 
+//MY SOLUTION
+
 function ArrayAdditionI(arr) {
   var sortedArr = arr.sort(function(a,b){return a - b;}).reverse();
   var largestNum = sortedArr.shift();
@@ -58,3 +60,50 @@ console.log(ArrayAdditionI([10,2,16,1,5]));
 //
 // Input:3,5,-1,8,12
 // Output:"true"
+
+
+// A MORE SAVVY WAY...
+
+// function ArrayAddition(arr) {
+//
+//   // get largest number and remove it from array
+//   var sum = Math.max.apply(null, arr);
+//   arr.splice(arr.indexOf(sum), 1);
+//
+//   // clever way to get rid of negative values
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//       sum -= arr[i];
+//       arr[i] = 0;
+//     }
+//   }
+//
+//   // table to track elements
+//   var table = [[]];
+//
+//   // fill first row
+//   for (var i = 1; i <= sum; i++)
+//     table[0][i] = false;
+//
+//   // fill first column
+//   for (var i = 0; i <= arr.length; i++) {
+//     table[i][0] = true;
+//     if (i !== arr.length)
+//       table.push([]);
+//   }
+//
+//   // dynamic programming solution
+//   for (var i = 1; i <= arr.length; i++) {
+//     for (var j = 1; j <= sum; j++) {
+//       table[i][j] = table[i-1][j];
+//       if (table[i][j] === false && j >= arr[i-1]) {
+//         table[i][j] = table[i][j] || table[i-1][j-arr[i-1]];
+//       }
+//     }
+//   }
+//
+//   return table[arr.length][sum];
+//
+// }
+//
+// ArrayAddition([1, 2, 6, 4, 7, 12]);
