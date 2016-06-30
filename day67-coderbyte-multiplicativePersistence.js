686a09600c87274e65d59bcab3f8b707db1365a4
blikethat <kamie.lyn@gmail.com>
2016-06-29 22:36:33 -0700

    multiplys  all the digits in a number until the product is an integer less than 10

---
+++ day67-coderbyte-multiplicativePersistence.js
@@ -0,0 +1,29 @@
+//Using the JavaScript language, have the function MultiplicativePersistence(num) take the num parameter being passed which will always be a positive integer and return its multiplicative persistence which is the number of times you must multiply the digits in num until you reach a single digit. For example: if num is 39 then your program should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and finally 1 * 4 = 4 and you stop at 4.
+
+function MultiplicativePersistence(num) {
+  var i = 0;
+  if (num < 10){
+    return i;
+  }
+  var numArr = num.toString().split("");
+  while(true){
+    i++;
+    var reducedNum = numArr.reduce(function(pre, cur){
+      return pre * cur;
+    });
+    if(reducedNum < 10){
+      return i;
+    }
+    numArr = reducedNum.toString().split("");
+  }
+}
+
+// keep this function call here
+console.log(MultiplicativePersistence(25));  
+
+
+// Input:4
+// Output:0
+//
+// Input:25
+// Output:2
