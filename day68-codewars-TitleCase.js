f0c80d55b4f09cbcb5589db1c814f4b1ba3b1518
blikethat <kamie.lyn@gmail.com>
2016-07-01 19:40:36 -0700

    A function that converts a string to title case taking two arguments—the string to convert and exempt words

--- day68-codewars-TitleCase.js
+++ day68-codewars-TitleCase.js
@@ -6,17 +6,25 @@
 function titleCase(title, exemptWords){
     // make the title argument lowercase and split the string into an array of words
     var titleArr = title.toLowerCase().split(" ");
-    // make the exemptWords lowercase and split them into an array of words
-    var exemptWords = exemptWords.toLowerCase().split(" ");
+    // if there is a second argument, make the exemptWords lowercase and split them into an array of words
+    if (exemptWords !== undefined){
+      var exemptWords = exemptWords.toLowerCase().split(" ");
+    }
     //map and return the title array
     return titleArr.map(function (el, index){
       //if it is the first word in the array, capitalize the first letter and return the word
-      if(titleArr[0] === titleArr[index]){
+      if(index === 0){
         return titleArr[0].charAt(0).toUpperCase() + el.substring(1, el.length);
       }
-      //On the next three lines I am trying to check an element in the titleArr array against the exemptWords parameter—which I defined as its own array. My inclination is to put a for loop in the if statement conditional to check el against each item in the array, but that just doesn't seem like the best idea. Any suggestions?
-      if(el === ){
-        return ;
+      //if the element matches one of the exemptWords, assign it to a variable skipMe
+      if (exemptWords !== undefined){
+        var skipMe = exemptWords.filter(function(el2){
+          return el === el2;
+        }).join();
+      }
+      //if skipMe equals the element, return it as is
+      if(el === skipMe){
+        return skipMe;
       }
       // for any other word in the array, capitalize the first letter and return it
       return el.charAt(0).toUpperCase() + el.substring(1, el.length);
@@ -24,7 +32,7 @@ function titleCase(title, exemptWords){
     }).join(" ");
 }
 
-console.log(titleCase('a clash of KINGS', 'a an the of'));
+console.log(titleCase('the quick brown fox'));
 
 
 
