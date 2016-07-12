e21406aa185fcb4e4d518fc15f8e2bc796b39d17
blikethat <kamie.lyn@gmail.com>
2016-07-11 22:12:10 -0700

    rebuilds for Math.round, ceiling, & floor

--- day74-codewars-mathIssues.js
+++ day74-codewars-mathIssues.js
@@ -1,28 +1,29 @@
 //Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
-//
+
 Math.round = function(number) {
-var breakItUp = number.toString().split("");
-console.log(breakItUp);
-if(parseInt(breakItUp[2]) >= 5){
-  return parseInt(breakItUp[0]) + 1;
-}
-return parseInt(breakItUp[0]);
+  var compare = parseInt(number.toString().charAt(number.toString().lastIndexOf(".") + 1));
+  var theNum = number.toString().split(".");
+  if(compare >= 5 && compare !== number){
+    return parseInt(theNum[0]) + 1;
+  }
+  return parseInt(theNum[0]);
 };
 
 Math.ceil = function(number) {
-  var breakItUp = number.toString().split("");
-  if(breakItUp[2] !== undefined){
-    return parseInt(breakItUp[0]) + 1;
+  var compare = parseInt(number.toString().charAt(number.toString().lastIndexOf(".") + 1));
+  var theNum = number.toString().split(".");
+  if(compare !== number){
+    return parseInt(theNum[0]) + 1;
   }
   return number;
 };
 
 Math.floor = function(number) {
-  var breakItUp = number.toString().split(".");
-  return parseInt(breakItUp[0]);
+  var theNum = number.toString().split(".");
+  return parseInt(theNum[0]);
 };
 
-console.log(Math.round(7.12));
+console.log(Math.round(5));
 
 // Test.assertEquals(Math.round(0.4), 0, 'Math.round(0.4)');
 // Test.assertEquals(Math.round(0.5), 1, 'Math.round(0.5)');
