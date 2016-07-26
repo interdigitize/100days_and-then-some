d2a28300714b7925127d51e4c525642b7b33852c
blikethat <kamie.lyn@gmail.com>
2016-07-25 20:44:37 -0700

    two functions. one returns the max number in an array. The other returns the min.

---
+++ day84-codewars-findMaxMin.js
@@ -0,0 +1,23 @@
+//Your task is to make two functions, max and min (maximum and minimum in PHP) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+var min = function(list){
+    list.sort(function(a,b){
+      return a - b;
+    });
+    return list[0];
+}
+
+var max = function(list){
+    list.sort(function(a,b){
+      return b - a;
+    });
+    return list[0];
+}
+
+console.log(max([4,6,2,1,9,63,-134,566]));
+
+
+// max([4,6,2,1,9,63,-134,566]) returns 566
+// min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+// max([5]) returns 5
+// min([42, 54, 65, 87, 0]) returns 0
