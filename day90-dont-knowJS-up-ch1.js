b1334111be8cf495ad562742faf480e7b19a3e1d
blikethat <kamie.lyn@gmail.com>
2016-08-12 23:48:13 -0700

    working

--- day90-dont-knowJS-up-ch1.js
+++ day90-dont-knowJS-up-ch1.js
@@ -5,30 +5,34 @@
 // You should define functions for calculating the tax and for formatting the price with a "$" and rounding to two decimal places.
 // Bonus Challenge: Try to incorporate input into this program, perhaps with the prompt(..) covered in "Input" earlier. You may prompt the user for their bank account balance, for example. Have fun and be creative!
 
-const taxRate = 0.09;
+const taxRate = 0.1;
 const phonePrice = 380.98;
 const accessoryPrice = 59.60;
 const spendingThreshold = 500;
 // var bankBalance = prompt("How much money do you have to spend in the bank?");
-var bankBalance = 5000;
-
-while(bankBalance > 0){ //infinite...ug
-  var price = phonePrice + accessoryPrice;
-  console.log(formatPrice(calculateTax(price)));
-  formatPrice(calculateTax(price));
-  if(calculateTax(price) > bankBalance){
-    console.log("Sorry, you are low on funds and can not purchase the items. If you are a spoiled child, call home. If you live in the realy world, keep saving.")
-  }
-  else{
-    bankBalance -= calculateTax(price);
-  }
-
-}
+var bankBalance = 1000;
+var total = 0;
 
 function calculateTax(price){
-  return price + (price * taxRate);
+  return price * taxRate;
 }
 
 function formatPrice(price){
-  return console.log("The cost of you phone purchase is: "+ "$" + Math.round(price * 100));
+  return "$" + price.toFixed(2);
+}
+
+while(bankBalance > total){ //infinite...ug
+  total += phonePrice + calculateTax(phonePrice);
+
+  var price = formatPrice(calculateTax(phonePrice));
+  if(total < spendingThreshold){
+    total += accessoryPrice + calculateTax(phonePrice);
+  }
+
+  console.log("Your purchase: " + formatPrice(total));
+
+  if(bankBalance < total){
+    console.log("Sorry, you are low on funds and can not purchase the items. If you are a spoiled child, call home. If you live in the realy world, keep saving.")
+  }
+
 }
