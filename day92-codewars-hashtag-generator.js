6bb321e0c879de1240c234fffe4f059ce1b99aad
blikethat <kamie.lyn@gmail.com>
2016-08-30 00:01:07 -0700

    and go...

---
+++ day92-codewars-hashtag-generator.js
@@ -0,0 +1,20 @@
+// The marketing team are spending way too much time typing in hashtags.
+// Let's help them with out own Hashtag Generator!
+//
+// Here's the deal:
+//
+// If the final result is longer than 140 chars it must return false.
+// If the input is a empty string it must return false.
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// Example Input to Output:
+//
+
+
+function generateHashtag (str) {
+}
+
+console.log(generateHashtag());
+// " Hello there thanks for trying my Kata" => "#HelloThereThanksForTryingMyKata"
+//
+// " Hello World " => "#HelloWorld"
