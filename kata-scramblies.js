6c781cf7422f73efe7ad30e5c47d37dce39a1b20
interdigitize <interdigitize@users.noreply.github.com>
2016-12-09 10:33:21 -0800

    a function that takes two strings and returns true if a portion of str1 can be rearranged to match str2 --- works but needs to be refactored for performance

--- kata-scramblies.js
+++ kata-scramblies.js
@@ -1,24 +1,5 @@
 //Write function scramble(str1,str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
 
-// function scramble(str1, str2) {
-//   var str1Arr = str1.split(""), str2Arr = str2.split("");
-//
-//      function letterPresent(letter){
-//        for(var i = 0; i < str1Arr.length; i++){
-//          if (letter === str1Arr[i]){
-//            return true;
-//          }
-//        }
-//      }
-//      return str2Arr.map(letterPresent).reduce(function(pre, cur){
-//        if (pre === true && pre === cur){
-//          return true;
-//        }
-//        return false;
-//      });
-// }
-
-//refactored
 function scramble(str1, str2) {
   //split strings into arrays
   var str1Arr = str1.split(""), str2Arr = str2.split("");
@@ -27,28 +8,30 @@ function scramble(str1, str2) {
 
   //this returns the final answer
   return str2Arr.reduce(function(trueOrFalse, letter){
-     console.log(trueOrFalse);
       var ans;
-       for(var i = 0; i < str1Arr.length; i++){
+      // true or false needs to be returned to the accumulator for the final answer. I moved this to the top so if it is already false, it won't run the other lines of code,
+      if(trueOrFalse === false){
+        return false;
+      }
+       // loops through each letter of disappearingArr
+       for(var i = 0; i < disappearingArr.length; i++){
+           // compares a str2Arr letter to a disappearingArr letter
            if (letter === disappearingArr[i]){
-             disappearingArr.splice(i, 1);
+             // if they are equal, it removes the letter
+             disappearingArr.splice(i, 1);  // I think this is the problem. It appears to remove all instances of a letter rather than just the first.
+             // sets the var ans === true
              ans = true;
-             console.log(letter);
-             console.log(disappearingArr);
+             break;
            }
        }
-
-       if(trueOrFalse === false){
-         return false;
-       }
-       else if (ans === true || disappearingArr === []){
+       // returns true if the letter was found and moves to the next letter
+       if (ans === true || disappearingArr === []){
          return true;
        }
+       // returns false
          return false;
      }, 0);
-
 }
-
 
 // For example:
 // str1 is 'rkqodlw' and str2 is 'world' the output should return true.
